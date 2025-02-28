adc879c6f97813ea1b92d3b24edb3d7808996ceb
JOSEPDAM\infci <jprodrigogarcia01.26@gmail.com>
2025-02-28 09:54:49 +0100

    Mostrar pasajeros

--- Constantes.py
+++ Constantes.py
@@ -7,3 +7,6 @@ port= '5432'
 QUERY_CLIENTES_ACTIVOS = 'SELECT * FROM scexamen.personas  WHERE estado = 1'
 QUERY_VUELOS_BY_PASAJERO = ('SELECT c.nombre as "Ciudad origen", c2.nombre as "Ciudad destino" FROM scexamen.vuelos v JOIN scexamen.usuariosvuelo uv ON v.id=uv.idvuelo JOIN scexamen.personas p ON p.id=uv.idpasajero '
                             'JOIN scexamen.ciudades c ON c.id=v.idciudadorigen JOIN scexamen.ciudades c2 ON c2.id=v.idciudaddestino WHERE p.id=%(id_pasajero)s')
+QUERY_VUELOS = 'SELECT * FROM scexamen.vuelos'
+QUERY_DATOS_VUELO_ID = ('SELECT p.nombre, p.apellidos, c.nombre as "Ciudad origen", c2.nombre as "Ciudad destino", uv.preciobillete as "Precio_billete" FROM scexamen.vuelos v JOIN scexamen.usuariosvuelo uv ON v.id=uv.idvuelo JOIN scexamen.personas p ON p.id=uv.idpasajero '
+                            'JOIN scexamen.ciudades c ON c.id=v.idciudadorigen JOIN scexamen.ciudades c2 ON c2.id=v.idciudaddestino WHERE v.id=%(id_vuelo)s')
--- principal.py
+++ principal.py
@@ -1,7 +1,8 @@
 from Administrador import Administrador
-from Constantes import QUERY_CLIENTES_ACTIVOS, QUERY_VUELOS_BY_PASAJERO
+from Constantes import QUERY_CLIENTES_ACTIVOS, QUERY_VUELOS_BY_PASAJERO, QUERY_VUELOS
 from Empleados import Empleados
-from Log import escribirError, msjMenu, errDato, escribirInfo, msjConsulta1, msjPermisos, errUserNull, errUserPermisos
+from Log import escribirError, msjMenu, errDato, escribirInfo, msjConsulta1, msjPermisos, errUserNull, errUserPermisos, \
+    msjConsulta2
 from Recepcionista import Recepcionista
 import pandas as pd
 from db import PostgreSQLConnection
@@ -34,9 +35,56 @@ def comprobar_usuario(empleados):
     print(f"No existe ningun usuario con el nombre {nick}")
     return None
 
+def todos_vuelos():
+    result, column_names = db_connection.execute_query(QUERY_VUELOS)
+    if result:
+        df = pd.DataFrame(result, columns=column_names)
+        print(df)
+
+def menu_opcion2(df):
+    while True:
+        print("\n--- MENÚ ---")
+        print("1. Usuario que más ha pagado")
+        print("2. Usuario que menos ha pagado")
+        print("3. Media del precio de los billetes")
+        print("4. Salir")
+        while True:
+            try:
+                opcion = int(input("Opcion elegida: "))
+                break
+            except ValueError:
+                escribirError(msjConsulta2, "TIPO DE DATO INTRODUCIDO INCORRECTO")
+                print("Opcion no valida")
+        if opcion == 1:
+            print(df[df['Precio_billete']==df.Precio_billete.max()])
+        elif opcion == 2:
+            print(df[df['Precio_billete']==df.Precio_billete.min()])
+        elif opcion == 3:
+            print(f"Media de precios del billete: {df.Precio_billete.mean()}")
+        elif opcion == 4: break
+        else:
+            print("Opcion no valida")
+
+def mostrar_datos_vuelo():
+    todos_vuelos()
+    while True:
+        try:
+            id = int(input("Introduce el id del vuelo: "))
+            break
+        except ValueError:
+            escribirError(msjConsulta2, errDato)
+            print("Introduce un numero entero")
+    result, column_names = db_connection.execute_query(QUERY_VUELOS_BY_PASAJERO, {'id_vuelo': id})
+    if result:
+        df = pd.DataFrame(result, columns=column_names)
+        print(df)
+        menu_opcion2(df)
+
+def asignar_mayor_menor():
+    print("Usuarios modificados")
+
 
 def menu():
-
     empleados = Empleados()
     #añadir aqui a los empleados el admin y el recepcionista
     #cada vez que se realice una actualización o una consulta se deberá sumar 1.
@@ -59,10 +107,38 @@ def menu():
         if opcion == 1:
             user = comprobar_usuario(empleados.empleados)
             if user != None:
-                if isinstance(user, Recepcionista):
-                    mostrar_vuelos()
-                    user.num_consultas += 1
+                if isinstance(user, Administrador):
+                    asignar_mayor_menor()
+                    user.num_actualizaciones += 1
                     escribirInfo(msjConsulta1)
+                else:
+                    escribirError(msjPermisos, errUserNull)
+                    print("Usuario invalido")
+            else:
+                escribirError(msjPermisos, errUserPermisos)
+                print("El usuario no existe")
+
+        elif opcion == 2:
+            user = comprobar_usuario(empleados.empleados)
+            if user != None:
+                if isinstance(user, Recepcionista):
+                    mostrar_datos_vuelo()
+                    user.num_consultas += 1
+                    escribirInfo(msjConsulta2)
+                else:
+                    escribirError(msjPermisos, errUserNull)
+                    print("Usuario invalido")
+            else:
+                escribirError(msjPermisos, errUserPermisos)
+                print("El usuario no existe")
+
+        elif opcion == 3:
+            user = comprobar_usuario(empleados.empleados)
+            if user != None:
+                if isinstance(user, Recepcionista):
+                    mostrar_datos_vuelo()
+                    user.num_consultas += 1
+                    escribirInfo(msjConsulta2)
                 else:
                     escribirError(msjPermisos, errUserNull)
                     print("Usuario invalido")
